5a3abd36405aaa5accad8278f3be195840e5799e
mczerwi <marcinczrw@gmail.com>
2015-01-15 23:41:23 +0100

    Ładniejsze testy porównywania obiektów.

--- src/test/java/pl/mczpk/med/sr/algorithm/SequenceItemTest.java
+++ src/test/java/pl/mczpk/med/sr/algorithm/SequenceItemTest.java
@@ -8,10 +8,8 @@ public class SequenceItemTest {
 
 	@Test
 	public void shouldBeEqual() {
-		SequenceItem item1 = new SequenceItem("a");
-		item1.addLastElement("b");
-		SequenceItem item2 = new SequenceItem("a");
-		item2.addLastElement("b");
+		SequenceItem item1 = new SequenceItem("a", "b");
+		SequenceItem item2 = new SequenceItem("a", "b");
 		
 		assertEquals(item1, item2);
 		assertEquals(item1.hashCode(), item2.hashCode());
--- src/test/java/pl/mczpk/med/sr/algorithm/SequenceTest.java
+++ src/test/java/pl/mczpk/med/sr/algorithm/SequenceTest.java
@@ -10,12 +10,8 @@ public class SequenceTest {
 	public void shouldBeEqual() {
 		SequenceItem item1 = new SequenceItem("a");
 		SequenceItem item2 = new SequenceItem("b");
-		Sequence s1 = new Sequence();
-		s1.addLastItem(item1);
-		s1.addLastItem(item2);
-		Sequence s2 = new Sequence();
-		s2.addLastItem(item1);
-		s2.addLastItem(item2);
+		Sequence s1 = new Sequence(item1, item2);
+		Sequence s2 = new Sequence(item1, item2);
 		
 		assertEquals(s1, s2);
 		assertEquals(s1.hashCode(), s2.hashCode());
